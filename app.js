ea9074ef2c0a9f80e2359e5c004c33d825963ffb
anaandradesilveira-cyber <ana.andrade.silveira@escola.pr.gov.br>
2026-05-12 20:44:10 -0300

    Refactor secret number game with random number

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 alert('boas vindas ao jogo do número secreto');
-let numeroSecreto = 2;
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;   
 // enquanto chute não for igual ao n.s
 while (chute != numeroSecreto){
-    chute = prompt('escolha um número entre 1 e 10');
+    chute = prompt(`escolha um número entre 1 e ${numeroMaximo}`);
 
     // se o numero for igual ao numero secreto
 if (chute == numeroSecreto){
